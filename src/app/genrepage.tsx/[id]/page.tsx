a739521b0b42d08ba7bd105b93cd32b65bf4ecc6
Zoljargal Dagiiraz <zolooo@G303-CC0161.local>
2025-10-09 18:07:19 +0800

    fixed genresfilter page

--- src/app/genrepage.tsx/[id]/page.tsx
+++ src/app/genrepage.tsx/[id]/page.tsx
@@ -5,17 +5,5 @@ export default function HomePage({
 }: {
   params: { id: string };
 }) {
-  return (
-    <div></div>
-    // <div>
-    //   <SectionTwo
-    //     isLoading={false}
-    //     imgH={"340px"}
-    //     width={230}
-    //     height={439}
-    //     //   title={"Popular"}
-    //     //   movies={popularMovies}
-    //   ></SectionTwo>
-    // </div>
-  );
+  return <div>{/* <SectionTwo></SectionTwo> */}</div>;
 }

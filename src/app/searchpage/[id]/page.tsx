ef38b2c43050a4e06ae21eefec08bc3377de1f3a
Zoljargal Dagiiraz <zolooo@G303-CC0161.local>
2025-10-13 18:23:54 +0800

    fixed genre filter page , created genre page and activated see more button , created search page and added features to clean up the code

--- src/app/searchpage/[id]/page.tsx
+++ src/app/searchpage/[id]/page.tsx
@@ -3,6 +3,7 @@ import { MovieGenres } from "@/app/genresfilter/_features/MovieGenres";
 import { genresType } from "@/lib/type";
 import { axiosInstance } from "@/lib/utils";
 import Link from "next/link";
+import { Results } from "./_components/Results";
 
 export default async function Home({
   params: { id },
@@ -19,6 +20,7 @@ export default async function Home({
     <div className="px-20 pt-13 pb-8 flex flex-col gap-8">
       <Header />
       <div className="flex gap-7">
+        <Results />
         <div className="border-[1px] mx-4"></div>
         <MovieGenres genres={genreName} />
       </div>
